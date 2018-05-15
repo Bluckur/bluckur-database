d4071bde2dfc97569944021c5508ddc1435b559d
Unknown <sebastiaanvgaans@hotmail.com>
2018-05-15 15:48:50 +0200

    GetGlobalState

--- database.js
+++ database.js
@@ -1,19 +1,41 @@
-const levelDB = require('./levelDB');
-const verbose = true;
+const LevelDB = require('./levelDB');
+const verbose = false;
 
 class Database {
     constructor(isBackupValidator = false) {
-        this.levelBlocks = new levelDB('../database/level/blocks', verbose);
-        this.levelGlobalState = new levelDB('../database/level/state', verbose);
+        this.levelBlocks = new LevelDB('../database/level/blocks', verbose);
+        this.levelGlobalState = new LevelDB('../database/level/state', verbose);
         this.isBackupValidator = isBackupValidator
     }
 
-    getFullBlockChain(){
+    getFullBlockChain() {
 
     }
+    addToGlobalState(key, value) {
+        return new Promise((resolve) => {
+            this.levelGlobalState.put(key, value)
+                .then(() => {
+                    resolve(true);
+                });
+        });
+    }
 
-    getFullGlobalstate(){
+    getFullGlobalstate() {
+        return new Promise((resolve) => {
+            let globalState = [];
 
+            this.levelGlobalState.getAll()
+                .on('data', function (data) {
+                    globalState.push(data);
+                    if (verbose) console.log(data.key, '=', data.value)
+                })
+                .on('error', function (err) {
+                    if (verbose) console.log('Error while fetching global state', err);
+                })
+                .on('end', function () {
+                    resolve(globalState);
+                });
+        });
     }
 }
 
