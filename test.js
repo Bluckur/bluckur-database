bdc0beddc1bbd13b2abd16ce7f942919e77e0a95
Vidovitsch <david.de.bekker@gmail.com>
2018-06-12 21:35:47 +0200

    blockchainLevelRepository.js JSON conversion added

--- test.js
+++ test.js
@@ -1,3 +1,35 @@
 const levelDB = require('./lib/levelDB/levelDB').createInstance();
+const Models = require('bluckur-models');
 
-levelDB.openBlockchainLevelAsync();
+const block1 = Models.createBlockInstance({
+  blockHeader: Models.createBlockHeaderInstance({
+    blockNumber: 2,
+    validator: '75821586eeee3dc40f7ef83df809c9a38de017ed3adaed8130becd267f5eeffb',
+    timestamp: +new Date(),
+    blockReward: this.BLOCK_REWARD,
+    parentHash: '0',
+  }),
+});
+
+const block2 = Models.createBlockInstance({
+  blockHeader: Models.createBlockHeaderInstance({
+    blockNumber: 3,
+    validator: '75821586eeee3dc40f7ef83df809c9a38de017ed3adaed8130becd267f5eeffb',
+    timestamp: +new Date(),
+    blockReward: this.BLOCK_REWARD,
+    parentHash: '0',
+  }),
+});
+
+levelDB.openBlockchainLevelAsync().then(() => {
+  console.log('b');
+  return levelDB.blockchainRepository.putBlockAsync(block1);
+}).then(() => {
+  console.log('a');
+  return levelDB.blockchainRepository.getBlockAsync(2);
+}).then((blocks) => {
+  console.log(blocks);
+})
+  .catch((err) => {
+    console.log(err);
+  });
